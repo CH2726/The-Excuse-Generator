42bd76fc68bde97c81a92954cd55bfde4e828d5e
chubbard91 <chubbard91@gmail.com>
2021-03-09 06:49:21 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  button = () => {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let wholoop = Math.floor(Math.random() * who.length);
+    let actionloop = Math.floor(Math.random() * action.length);
+    let whatloop = Math.floor(Math.random() * what.length);
+    let whenloop = Math.floor(Math.random() * when.length);
+
+    // return (document.querySelector("#excuse").innerHtml = who[0]);
+    // who[wholoop] +
+    // "" +
+    // action[actionloop] +
+    // "" +
+    // what[whatloop] +
+    // "" +
+    // when[whenloop] +
+    // "");
+  };
 };
+
+// check out this math floor random example: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_random
