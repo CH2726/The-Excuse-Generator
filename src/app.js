b7f3e4874ff12c8f022e7a30046d020ddf537f43
chubbard91 <chubbard91@gmail.com>
2021-03-10 01:04:11 +0000

    edits to .js working with Alejandro

--- src/app.js
+++ src/app.js
@@ -6,33 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  button = () => {
-    let who = ["The dog", "My grandma", "His turtle", "My bird"];
-    let action = ["ate", "peed", "crushed", "broke"];
-    let what = ["my homework", "the keys", "the car"];
-    let when = [
-      "before the class",
-      "right on time",
-      "when I finished",
-      "during my lunch",
-      "while I was praying"
-    ];
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  let excuse = document.querySelector("h3");
 
-    let wholoop = Math.floor(Math.random() * who.length);
-    let actionloop = Math.floor(Math.random() * action.length);
-    let whatloop = Math.floor(Math.random() * what.length);
-    let whenloop = Math.floor(Math.random() * when.length);
-
-    // return (document.querySelector("#excuse").innerHtml = who[0]);
-    // who[wholoop] +
-    // "" +
-    // action[actionloop] +
-    // "" +
-    // what[whatloop] +
-    // "" +
-    // when[whenloop] +
-    // "");
-  };
+  excuse.innerHTML =
+    who[whoIndex] + " " + action[actionIndex] + " " + " " + when[whenIndex];
 };
-
-// check out this math floor random example: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_random
